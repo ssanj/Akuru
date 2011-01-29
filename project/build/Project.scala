a85b7e9f174296d52dc341d82921c7c60d47d87c
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-01-29 21:57:33 +1000

    added test suites

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,5 +3,6 @@ import sbt._
 class Project(info:ProjectInfo) extends DefaultProject(info) {
   lazy val artifactory = "Artifactory Release" at "http://hyperion:9080/artifactory/libs-releases"
   lazy val mongoDriver = "org.mongodb" % "mongo-java-driver" % "2.4" withSources()
+  lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" withSources()
 }
 
