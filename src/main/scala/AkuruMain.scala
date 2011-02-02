a4328d472884392a5872ef0bef9716e98886c904
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-02-02 17:17:46 +1000

    Added implicit CollectionName[T] parameter to save

--- src/main/scala/AkuruMain.scala
+++ src/main/scala/AkuruMain.scala
@@ -11,37 +11,37 @@ object AkuruMain extends DomainObjects with Tools with SideEffects {
 
 
   def main(args: Array[String]) {
-    val labelSeq = List("work", "movement")
-    val blog = Blog(title = "Lessons learned", labels = labelSeq.toSeq)
-    val result = (withAkuru
-                    on("blog", save(blog) _)
-                    on("label", labelSeq map (l => save(Label(value = l)) _))
+    val labelList = List("work", "movement")
+    val blog = Blog(title = "Lessons learned", labels = labelList.toSeq)
+    val result = (withAkuru ->
+                    (save(blog)) ->>
+                    (labelList.map(l => save(Label(value = l)) _))
                   run) getOrElse("success >>")
     println(result)
   }
 
-  def withAkuru(): FutureConnection = withConnection(createServer)("akuru")
+  def withAkuru: FutureConnection = withConnection(createServer)("akuru")
 
   def withConnection(s:() => MongoServer)(dbName:String): FutureConnection =  FutureConnection(s, dbName)
 
   def createServer = () => new MongoServer
 
-  def save[T <% MongoObject : CollectionName](f:  => T)(col:MongoCollection):Option[String] = { col.save3(f) }
+  def save[T <% MongoObject : CollectionName](f:  => T)(col:String => MongoCollection):Option[String] = {
+    col(implicitly[CollectionName[T]].name).save3(f)
+  }
 
-  type UserFunction = MongoCollection => Option[String]
+  type UserFunction = (String => MongoCollection) => Option[String]
 
-  type ColToUserFunction = (String, UserFunction)
+  case class FutureConnection(fserver:() => MongoServer, dbName:String, items:List[UserFunction] = Nil) {
 
-  case class FutureConnection(fserver:() => MongoServer, dbName:String, items:List[ColToUserFunction] = Nil) {
+    def ->(uf:UserFunction): FutureConnection = ->>(List(uf))
 
-    def on(col:String, uf:UserFunction): FutureConnection = on(col, List(uf))
-
-    def on(col:String, f:List[UserFunction]): FutureConnection = f.foldLeft(this)((a,b) => FutureConnection(fserver, dbName, (col, b) :: items))
+    def ->>(f:List[UserFunction]): FutureConnection = FutureConnection(fserver, dbName,  f ::: items)
 
     def run: Option[String] = {
       runSafelyWithDefault{
         getServer.right.flatMap(getDatabase(_).right.map(db => items.foldRight(None:Option[String]){(t, a) =>
-          if (!a.isDefined) t._2(db.getCollection(t._1)) else a})) match {
+          if (!a.isDefined) t(db.getCollection(_)) else a})) match {
           case Right(e @ Some(error)) => e
           case Right(n @ None) => n
           case Left(e @ error) => Some(e)
