00a0eb6eb0661b61b9981303f7180b823d11e3fd
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-02-02 20:27:14 +1000

    updated formatted

--- src/main/scala/AkuruMain.scala
+++ src/main/scala/AkuruMain.scala
@@ -13,10 +13,13 @@ object AkuruMain extends DomainObjects with Tools with SideEffects {
   def main(args: Array[String]) {
     val labelList = List("work", "movement")
     val blog = Blog(title = "Lessons learned", labels = labelList.toSeq)
-    val result = (withAkuru ->
+    val blogs:List[Blog] = (for (n <- 1 to 100) yield Blog(title = "Lessons learned" + n , labels = labelList.toSeq)).toList
+
+    val result = {withAkuru ->
                     (save(blog)) ->>
-                    (labelList.map(l => save(Label(value = l)) _))
-                  run) getOrElse("success >>")
+                    (labelList.map(l => save(Label(value = l)) _)) ->>
+                    (blogs.map(b => save(b) _))
+                  run}.getOrElse("success >>")
     println(result)
   }
 
