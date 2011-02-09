ab018b1fb8e946eafcecba79a0af1e862c6a948a
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-02-09 14:47:50 +1000

    removed redundant methods from MongoCollection

--- src/main/scala/MongoCollectionTrait.scala
+++ src/main/scala/MongoCollectionTrait.scala
@@ -16,28 +16,7 @@ trait MongoCollectionTrait extends Tools {
     import org.bson.types.ObjectId
     import com.mongodb.{DBObject, WriteResult}
 
-    def findOne[T](mo:MongoObject)(implicit con:MongoConverter[T]): Either[MongoError, Option[T]] = {
-      wrapWith{ newdbc.findOne(mo.toDBObject).map(con.convert(_)) }
-    }
-
-    def find[T](mo:MongoObject)(implicit con:MongoConverter[T]): Either[MongoError, Seq[T]] = {
-      wrapWith{
-        val mc:MongoCursor = newdbc.find(mo.toDBObject)
-        mc.toSeq[T]
-      }
-    }
-
-    def save(mo:MongoObject): Either[MongoError, Unit] = {
-      import MongoTypes.MongoWriteResult._
-      dbc.save(mo.toDBObject).getMongoError match {
-        case None => Right()
-        case Some(me) => Left(me)
-      }
-    }
-
-    def save[T](value:T)(implicit mc:MongoConverter[T]): Either[MongoError, Unit] =  save(mc.convert(value))
-
-    def save3[T <% MongoObject](value: => T): Option[String] =  writeResultToOption(() => dbc.save(value.toDBObject))
+   def save3[T <% MongoObject](value: => T): Option[String] =  writeResultToOption(() => dbc.save(value.toDBObject))
 
     def findOne3[T](mo:MongoObject)(implicit f3: MongoObject => T): Either[String, Option[T]] = {
       runSafelyWithEither { nullToOption(dbc.findOne(mo.toDBObject)).map(t => f3(t)) }
@@ -60,16 +39,6 @@ trait MongoCollectionTrait extends Tools {
     }
 
 
-    def update(query:MongoObject, upate:MongoObject, upsert:Boolean):Either[MongoError, Unit] = {
-      import MongoTypes.MongoWriteResult._
-      wrapWith {
-        dbc.update(query.toDBObject, upate.toDBObject, upsert, false)
-      } match {
-        case Right(result) => result.getMongoError match { case None => Right();  case Some(me) => Left(me) }
-        case Left(me) => Left(me)
-      }
-    }
-
     def findAndModify[T](query:MongoObject, sort:MongoObject, update:MongoObject, returnNew:Boolean)(implicit mc:MongoConverter[T]):
       Either[MongoError, T] = {
       import MongoObject.empty
@@ -77,8 +46,6 @@ trait MongoCollectionTrait extends Tools {
         mc.convert(dbc.findAndModify(query, empty, sort, false, update, returnNew, false))
       }
     }
-
-    def drop: Either[MongoError, Unit] = wrapWith(dbc.drop)
 
     def drop3: Option[String] = runSafelyWithOptionReturnError(dbc.drop)
   }
