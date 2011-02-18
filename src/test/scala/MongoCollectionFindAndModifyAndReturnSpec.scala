e0e6568ad0a0e675fb881e410b066a9a21807db2
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-02-18 17:44:39 +1000

    added tests where the updates where

--- src/test/scala/MongoCollectionFindAndModifyAndReturnSpec.scala
+++ src/test/scala/MongoCollectionFindAndModifyAndReturnSpec.scala
@@ -4,22 +4,25 @@
  */
 package akuru
 
-final class MongoCollectionFindAndModifySpec extends CommonSpec {
+final class MongoCollectionFindAndModifyAndReturnSpec extends CommonSpec {
 
   import Blog._
-  import MongoTypes.MongoObject.empty
+  import MongoTypes.MongoObject._
   "A MongoCollection with findAndModify" should "find and modify an existing object" in {
     (onTestDB ~~>
             drop[Blog] ~~>
             save(Blog(title = titleField("Parry Hotter"), labels = labelsField(Seq("book", "movie")))) ~~>
-            findAndModifyAndReturn(Blog.titleField("Parry Hotter"))(empty) {
+            findAndModifyAndReturn(titleField("Parry Hotter"))(empty) {
               Blog(title = titleField("Harry Potter"), labels = labelsField(Seq("books", "movies"))) }{ b:Blog =>
               b.title.value should equal ("Harry Potter")
               b.labels.value should equal (Seq("books", "movies"))
               success
-            } {
-              Some("Parry Hotter was not updated!!")
-            } ~~>()
+            } { Some("Parry Hotter was not updated!!") } ~~>
+            findAndModifyAndReturn(titleField("Harry Potter"))(empty)( set(titleField("Rahhy Ropper"))) { b: Blog =>
+              b.title.value should equal ("Rahhy Ropper") //only title has changed
+              b.labels.value should equal (Seq("books", "movies")) //has not changed
+              success
+            } { Some("Harry Potter was not updated!!") } ~~>()
     ) verifySuccess
   }
 }
