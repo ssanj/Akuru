6ccecc02f0ba6215ef239af18bd0bc49714df349
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-02-26 19:15:35 +1000

    added test for mergingDupes

--- src/test/scala/MongoObjectwithMergeDupesSpec.scala
+++ src/test/scala/MongoObjectwithMergeDupesSpec.scala
@@ -10,7 +10,7 @@ final class MongoObjectwithMergeDupesSpec extends CommonSpec {
   import MongoTypes.MongoObject
   import MongoTypes.MongoObject.mongo
 
-  "A MongoObject" should "should merge documents with no dupes" in {
+  "A MongoObject" should "merge documents with no dupes" in {
     val mo1:MongoObject = mongo.putPrimitive(titleField("blah"))
     val mo2:MongoObject = mongo.putPrimitiveArray[String](labelsField(Seq("test")))
     val mo3 = mo1.mergeDupes(mo2)
@@ -24,5 +24,17 @@ final class MongoObjectwithMergeDupesSpec extends CommonSpec {
     val mo2:MongoObject = mongo.putMongo("$sort", mongo.putPrimitive("key2", "bleee"))
     val mo3 = mo1.mergeDupes(mo2)
     mo3 should equal (mongo.putMongo("$sort", mongo.putPrimitive("key1", "blah").putPrimitive("key2", "bleee")))
+  }
+
+  it should "merge dupes and non-dupes" in {
+    val mo1:MongoObject = mongo.putMongo("$sort", mongo.putPrimitive("key1", "blah")).putPrimitive("key3", "blue").putPrimitive("key4", "boo")
+    val mo2:MongoObject = mongo.putPrimitive("key5", "test").putMongo("$sort", mongo.putPrimitive("key2", "bleee")).putPrimitive("key6", "done")
+    val mo3 = mo1.mergeDupes(mo2)
+
+    mo3.getMongo("$sort") should equal (Some(mongo.putPrimitive("key1", "blah").putPrimitive("key2", "bleee")))
+    mo3.getPrimitive[String]("key3") should equal ("blue")
+    mo3.getPrimitive[String]("key4") should equal ("boo")
+    mo3.getPrimitive[String]("key5") should equal ("test")
+    mo3.getPrimitive[String]("key6") should equal ("done")
   }
 }
