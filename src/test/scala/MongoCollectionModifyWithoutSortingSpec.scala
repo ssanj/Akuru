2e8e107fb7cbb204cb06c972bb1987416e91ed44
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-03-17 15:32:03 +1000

    added test for modify upsert without sorting

--- src/test/scala/MongoCollectionModifyWithoutSortingSpec.scala
+++ src/test/scala/MongoCollectionModifyWithoutSortingSpec.scala
@@ -8,7 +8,7 @@ final class MongoCollectionModifyWithoutSortingSpec extends CommonSpec with Modi
 
   import Blog._
   import MongoTypes.MongoObject._
-  "A MongoCollection with findAndModify" should "find and modify an existing object" in {
+  "A MongoCollection with modify and no sorting" should "find and modify an existing object" in {
     ( initBlog ~~>
             save(Blog(titleField("Parry Hotter"), labelsField(Seq("book", "movie")))) ~~>
             ( modify a Blog where titleField === "Parry Hotter"  updateWith Blog(titleField("Harry Potter"), labelsField(Seq("books", "movies")))
@@ -18,11 +18,24 @@ final class MongoCollectionModifyWithoutSortingSpec extends CommonSpec with Modi
                       success }
                     onError error("Parry Hotter was not updated!!")
             ) ~~>
-            ( modify a Blog where titleField === "Harry Potter" updateWith set(titleField === "Rahhy Ropper") withUpdated { b =>
-              b.title.value should equal ("Rahhy Ropper") //only title has changed
-              b.labels.value should equal (Seq("books", "movies")) //has not changed
-              success
-            } onError error("Harry Potter was not updated!!") ) ~~>()
-    ) verifySuccess
+            ( modify a Blog where titleField === "Harry Potter" updateWith set(titleField === "Rahhy Ropper")
+                    withUpdated { b =>
+                      b.title.value should equal ("Rahhy Ropper") //only title has changed
+                      b.labels.value should equal (Seq("books", "movies")) //has not changed
+                      success
+                    }
+              onError error("Harry Potter was not updated!!") )
+    ) ~~>() verifySuccess
+  }
+
+  it should "upsert a Ddomain object that does not exist" in  {
+    ( initBlog ~~>
+        ( modify a Blog where titleField === "The Two Towers" upsertWith Blog(titleField === "The Two Towers")
+                withUpserted {b =>
+                  b.title.value should equal ("The Two Towers")
+                  success
+                }
+          onError error("Could not find the Two Towers") )
+    ) ~~>() verifySuccess
   }
 }
