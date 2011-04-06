a1de10071302de4fb6cae954bbe2d49f870182b9
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-04-06 12:32:06 +1000

    updated to use the mongo 2.5.2 driver

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class Project(info:ProjectInfo) extends DefaultProject(info) {
   lazy val artifactory = "Artifactory Release" at "http://hyperion:9080/artifactory/libs-releases"
-  lazy val mongoDriver = "org.mongodb" % "mongo-java-driver" % "2.4" withSources()
+  lazy val mongoDriver = "org.mongodb" % "mongo-java-driver" % "2.5.2" withSources()
   lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" withSources()
 
   override def compileOptions = CompileOption("-unchecked") :: CompileOption("-encoding") :: CompileOption("UTF-8") :: super.compileOptions.toList
