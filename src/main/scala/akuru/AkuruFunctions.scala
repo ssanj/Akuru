f90f96afe046515f56120aded3890925b5f93ee4
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-04-11 15:05:42 +1000

    refactored execute()

--- src/main/scala/akuru/AkuruFunctions.scala
+++ src/main/scala/akuru/AkuruFunctions.scala
@@ -13,7 +13,9 @@ trait AkuruFunctions {
   /**
    * A basic type that works on a DomainObject T and returns a result R.
    */
-  type WorkUnit[T <: DomainObject, R] = MongoServer => Either[String, R]
+  type WorkUnit[T <: DomainObject, R] = ConnectionProvider => Either[String, R]
+
+  type ConnectionProvider = (String, String) => MongoCollection
 
   trait ConnectionType[T <: DomainObject] {
     val colName:String
@@ -22,13 +24,13 @@ trait AkuruFunctions {
 
   case class Executor[T <: DomainObject, R](wu: WorkUnit[T, R]) {
 
-    def map[S](f: R => S): Executor[T, S] = Executor[T, S](server => wu(server).fold(l => Left(l), r => Right(f(r))))
+    def map[S](f: R => S): Executor[T, S] = Executor[T, S](cp => wu(cp).fold(l => Left(l), r => Right(f(r))))
 
-    def flatMap[U <: DomainObject, S](f: R => Executor[U, S]): Executor[U, S] = Executor[U, S](server => wu(server).fold(l => Left(l),
-      r => f(r).wu(server).fold(l2 => Left(l2), r2 => Right(r2))))
+    def flatMap[U <: DomainObject, S](f: R => Executor[U, S]): Executor[U, S] = Executor[U, S](cp => wu(cp).fold(l => Left(l),
+      r => f(r).wu(cp).fold(l2 => Left(l2), r2 => Right(r2))))
 
     def execute(implicit server: Either[String, MongoServer]): Either[String, R] = {
-      runSafelyWithEither{ server.right.flatMap (s => wu(s)) } match {
+      runSafelyWithEither{ server.right.flatMap (s => wu((db,col) => s.getDatabase(db).getCollection(col)) ) } match {
         case Left(error) => Left(error)
         case Right(result) => result
       }
