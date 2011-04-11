2fc95185a7a1ca39f1fdc0aea18caddda7d0af9b
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-04-11 12:27:04 +1000

    added map and flatMap functions

--- src/main/scala/akuru/AkuruFunctions.scala
+++ src/main/scala/akuru/AkuruFunctions.scala
@@ -16,6 +16,12 @@ trait AkuruFunctions {
   type WorkUnit[T] = CollectionProvider => Either[String, T]
 
   case class Executor[T](wu: WorkUnit[T]) {
+
+    def map[U](f: T => U): Executor[U] = Executor[U](cp => wu(cp).fold(l => Left(l), r => Right(f(r))))
+
+    def flatMap[U](f: T => Executor[U]): Executor[U] = Executor[U](cp => wu(cp).fold(l => Left(l), r => f(r).wu(cp).fold(l2 => Left(l2),
+      r2 => Right(r2))))
+
     def execute(implicit server: Either[String, MongoServer]): Either[String, T] = {
       runSafelyWithEither{ server.right.flatMap (s => wu(s.getDatabase("").getCollection(_))) } match {
         case Left(error) => Left(error)
